9a5ec8b46ac2c1775a6f04117ab8f52688937790
StealthWorm <thierrypitela@hotmail.com>
2024-05-25 09:38:58 -0300

    fix: added publicPath to redirect build resources

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -9,6 +9,9 @@ const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
+    // publicPath sera usada sempre que tiver uma parte do webpack que tenta referenciar um arquivo que passou pelo build
+    // como no nosso bucket da S3 definimos que os arquivos em produção serão redirecionados para o caminho "/container/latest", setamos este caminho como publicPath
+    publicPath: '/container/latest/',
   },
   plugins: [
     new ModuleFederationPlugin({
