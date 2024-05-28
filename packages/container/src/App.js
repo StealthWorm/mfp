e4b2d80943bf326767bd3532cb7f0bf8214b65d2
StealthWorm <thierrypitela@hotmail.com>
2024-05-28 09:25:22 -0300

    feat: added header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
 
 export default () => {
   return (
-    <div>
-      <h1>Container</h1>
-      <h2>Content added into a new branch</h2>
-      <hr />
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
   );
 };
