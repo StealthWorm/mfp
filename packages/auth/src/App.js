c83cdb1edcfce0547c370d7d11a15da072a8e07b
StealthWorm <thierrypitela@hotmail.com>
2024-06-05 19:38:30 -0300

    feat: configured basic auth into the container + configured auth deploy process

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -5,8 +5,8 @@ import {
   createGenerateClassName,
 } from '@material-ui/core/styles';
 
-import SignIn from './components/SignIn';
-import SignUp from './components/SignUp';
+import SignIn from './components/Signin';
+import SignUp from './components/Signup';
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'au',
